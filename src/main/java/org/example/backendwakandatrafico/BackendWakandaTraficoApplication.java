475a7b47f8a3e900cf72462b400b9130e9139789
marcos <marcgabe@myuax.com>
2024-11-26 19:50:03 +0100

    Initial commit

---
+++ src/main/java/org/example/backendwakandatrafico/BackendWakandaTraficoApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandatrafico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaTraficoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWakandaTraficoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backendwakandatrafico/BackendWakandaTraficoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandatrafico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaTraficoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
